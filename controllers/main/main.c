cdf0cd182a1c76ea755eb380453f4a2be595ea4f
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-07-09 21:57:11 +0200

    Refactor inverse kinematics function to return angles as a struct and update main to utilize calculated angles for motor movement; add .gitignore for build artifacts

--- controllers/main/main.c
+++ controllers/main/main.c
@@ -8,8 +8,14 @@
 
 #define TIME_STEP 32
 
+struct Angles {
+    double angleThigh;
+    double angleKnee;
+    double angleFoot;
+};
+
 // Only 2d for this time
-void inverseKinematicLeg(double x, double y, double z) {
+struct Angles inverseKinematicLeg(double x, double y, double z) {
      // cinématique inversée jambe
     int femur = 9.3;
     int tibia = 9.3;
@@ -19,17 +25,16 @@ void inverseKinematicLeg(double x, double y, double z) {
     
     if (y > max_length) {
         printf("Error: Position out of reach.\n");
-        return;
     }
 
-    double original_x = x;
+    //double original_x = x;
     x = x - LF;
 
-    // Hypoténus formé par les coordonnées x;y
-    double h = sqrt(x * x + y * y);
+    // Hypoténus formé par les coordonnées x;z
+    double h = sqrt(x * x + z * z);
 
-    // angle formé par les coordonnées x;y
-    double A1 = atan2(y, x);
+    // angle formé par les coordonnées x;z
+    double A1 = atan2(z, x);
 
     // Angle formé près du sol
     double A2 = acos((tibia * tibia + h * h - femur * femur) / (2 * tibia * h));
@@ -38,13 +43,28 @@ void inverseKinematicLeg(double x, double y, double z) {
     double A3 = acos((femur * femur + h * h - tibia * tibia) / (2 * femur * h));
 
     // angle coordonnées proche du bassin
-    double A4 = acos((y * y + h * h - x * x) / (2 * y * h));
+    double A4 = acos((z * z + h * h - x * x) / (2 * z * h));
 
     // angle du genou
     double A5 = acos((femur * femur + tibia * tibia - h * h) / (2 * femur * tibia));
 
     double angleThigh = A3 + A4;
-    double angleKnee = A5;
+    double angleKnee = 180 - A5;
+    double angleFoot = A2;
+
+    // Convertir les angles en degrés
+    struct Angles angles;
+    angles.angleThigh = angleThigh * 180.0 / M_PI;
+    angles.angleKnee = angleKnee * 180.0 / M_PI;
+    angles.angleFoot = angleFoot * 180.0 / M_PI;
+
+    // Afficher les angles
+    printf("Inverse Kinematics Angles:\n");
+    printf("Thigh Angle: %.2f degrees\n", angles.angleThigh);
+    printf("Knee Angle: %.2f degrees\n", angles.angleKnee);
+    printf("Foot Angle: %.2f degrees\n", angles.angleFoot);
+
+    return angles;
 
 }
 
@@ -99,6 +119,18 @@ int main() {
 
     // tête
     moveMotor(motors.head.Head, 20);
+
+    double x = 5.0, y = 0.0, z = 10.0;
+
+    // Inverse kinematics example
+    struct Angles legAngles = inverseKinematicLeg(x, y, z);
+    if (legAngles.angleThigh != 0 || legAngles.angleKnee != 0 || legAngles.angleFoot != 0) {
+        moveMotor(motors.leg.LegUpperR, legAngles.angleThigh);
+        moveMotor(motors.leg.LegLowerR, legAngles.angleKnee);
+        moveMotor(motors.leg.AnkleR, legAngles.angleFoot);
+    } else {
+        printf("Inverse kinematics failed.\n");
+    }
 
 
     // Simulation loop
