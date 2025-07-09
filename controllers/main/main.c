b2f0393598fce1b35df6cd925701d747a4dfb7e2
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-07-09 15:48:57 +0200

    Update motor initialization in main.c to set default positions for additional motors

--- controllers/main/main.c
+++ controllers/main/main.c
@@ -19,25 +19,56 @@ int main() {
     wb_keyboard_enable(TIME_STEP);
 
     // Initialiser les moteurs à leur position par défaut
+
     // épaules
     moveMotor(motors.arm.ShoulderR, 0);
     moveMotor(motors.arm.ShoulderL, 0);
 
     // bras supérieurs
-    moveMotor(motors.arm.ArmUpperR, 0);
-    moveMotor(motors.arm.ArmUpperL, 0);
+    moveMotor(motors.arm.ArmUpperR, 30);
+    moveMotor(motors.arm.ArmUpperL, 30);
 
     // bras inférieurs
     moveMotor(motors.arm.ArmLowerR, 70);
     moveMotor(motors.arm.ArmLowerL, 70);
 
+    // bassin
+    moveMotor(motors.pelvis.PelvYR, 0);
+    moveMotor(motors.pelvis.PelvYL, 0);
+    moveMotor(motors.pelvis.PelvR, 0);
+    moveMotor(motors.pelvis.PelvL, 0);
+
+    // jambes supérieures
+    moveMotor(motors.leg.LegUpperR, 25);
+    moveMotor(motors.leg.LegUpperL, 25);
+
+    // jambes inférieures
+    moveMotor(motors.leg.LegLowerR, 20);
+    moveMotor(motors.leg.LegLowerL, 20);
+
+    //chevilles
+    // moveMotor(motors.leg.AnkleR, 10);
+    // moveMotor(motors.leg.AnkleL, 10);
+
+    // pieds
+    moveMotor(motors.leg.FootR, 0);
+    moveMotor(motors.leg.FootL, 0);
+
+    // cou
+    moveMotor(motors.head.Neck, 0);
+
+    // tête
+    moveMotor(motors.head.Head, 20);
+
+
+
     // Simulation loop
     while (wb_robot_step(TIME_STEP) != -1) {
         int key = wb_keyboard_get_key();
         if (key == WB_KEYBOARD_UP) {
-            moveMotor(motors.arm.ShoulderL, wb_motor_get_target_position(motors.arm.ShoulderL) * 180.0 / M_PI + 10);
+            moveMotor(motors.leg.LegUpperR, wb_motor_get_target_position(motors.leg.LegUpperR) * 180.0 / M_PI + 10);
         } else if (key == WB_KEYBOARD_DOWN) {
-            moveMotor(motors.arm.ShoulderL, wb_motor_get_target_position(motors.arm.ShoulderL) * 180.0 / M_PI - 10);
+            moveMotor(motors.leg.LegUpperR, wb_motor_get_target_position(motors.leg.LegUpperR) * 180.0 / M_PI - 10);
         }
     }
 
