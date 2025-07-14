2ea2c6fbb2647d55a4e1ab305bf251f897bd5f4f
NotPunchnox <punchnoxpro@gmail.com>
2025-07-14 13:00:01 +0200

    Update build configuration; include position sensor header in main.d

--- controllers/main/build/release/main.d
+++ controllers/main/build/release/main.d
@@ -5,4 +5,5 @@ build/release/main.o: main.c \
  /usr/local/webots/include/controller/c/webots/motor.h \
  /usr/local/webots/include/controller/c/webots/gyro.h \
  /usr/local/webots/include/controller/c/webots/accelerometer.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h \
  src/motor.h
